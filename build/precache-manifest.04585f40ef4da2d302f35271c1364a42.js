1dfbafa9f2d5f94e8040ff96dfff79200b80496a
Sadurathman <sadurathman@gmail.com>
2023-03-15 07:58:37 +0530

    Initial View

---
+++ build/precache-manifest.04585f40ef4da2d302f35271c1364a42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8f09614c2b33c62f156",
+    "url": "/static/css/main.27aa6920.chunk.css"
+  },
+  {
+    "revision": "c8f09614c2b33c62f156",
+    "url": "/static/js/main.c8f09614.chunk.js"
+  },
+  {
+    "revision": "5ffc4f38957a8452797a",
+    "url": "/static/js/1.5ffc4f38.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7f2869dd3a1d9d380a9832b20a1c0144",
+    "url": "/index.html"
+  }
+];
